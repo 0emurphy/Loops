04fa5ff59f165af1303c0108671b9c7f0934a78b
0emurphy <31480914+0emurphy@users.noreply.github.com>
2017-09-15 10:12:32 -0500

    Add files via upload

--- loops.js
+++ loops.js
@@ -15,15 +15,18 @@ console.log("Done son!")
 
 function loopTwo(number, power){
 let sum = 0;
-for(sum >= 0; sum < power){
-let sum = number + sum
-let number = number + 1
+for(let p=0; p <=power; p++){
+  sum = sum + Math.pow(number,power)
+  number = number ++
 }
 return sum
 }
 
-function loopThree(){
-
+function loopThree(symbol,number){
+for(let p=0; p<=number;){
+  console.log(symbol);
+  symbol=symbol++
+}
 
 }
 
